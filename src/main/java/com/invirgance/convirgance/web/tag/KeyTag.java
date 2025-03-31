e1cc37f2fea5e2f76537da7fe697bd2d194c79aa
jbanes <jbanes@gmail.com>
2025-03-31 17:15:54 -0500

    First revision of ObjectTag and KeyTag

---
+++ src/main/java/com/invirgance/convirgance/web/tag/KeyTag.java
@@ -0,0 +1,104 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2025 jbanes.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.invirgance.convirgance.web.tag;
+
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.tagext.Tag;
+import jakarta.servlet.jsp.tagext.TagSupport;
+
+/**
+ *
+ * @author jbanes
+ */
+public class KeyTag extends TagSupport
+{
+    private String name;
+    private Object value;
+    private Object defaultValue;
+    
+    private Tag parent;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Object getValue()
+    {   
+        return value;
+    }
+
+    public void setValue(Object value)
+    {
+        this.value = value;
+    }
+
+    public Object getDefault()
+    {
+        return defaultValue;
+    }
+
+    public void setDefault(Object defaultValue)
+    {
+        this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public Tag getParent()
+    {
+        return this.parent;
+    }
+
+    @Override
+    public void setParent(Tag parent)
+    {
+        this.parent = parent;
+    }
+
+    @Override
+    public int doEndTag() throws JspException
+    {
+        if(getParent() instanceof ObjectTag)
+        {
+            ((ObjectTag)getParent()).set(name, value != null ? value : defaultValue);
+        }
+        
+        return EVAL_PAGE;
+    }
+    
+    @Override
+    public void release()
+    {
+        this.name = null;
+        this.value = null;
+        this.defaultValue = null;
+        
+        super.release();
+    }
+}
---
+++ src/main/java/com/invirgance/convirgance/web/tag/ObjectTag.java
@@ -0,0 +1,125 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2025 jbanes.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.invirgance.convirgance.web.tag;
+
+import com.invirgance.convirgance.json.JSONObject;
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.PageContext;
+import jakarta.servlet.jsp.tagext.Tag;
+import jakarta.servlet.jsp.tagext.TagSupport;
+
+/**
+ *
+ * @author jbanes
+ */
+public class ObjectTag extends TagSupport
+{
+    private String variable;
+    private String scope = "page";
+    private JSONObject object;
+    
+    private Tag parent;
+
+    private int getScopeInt() throws JspException
+    {
+        switch(scope)
+        {
+            case "page": return PageContext.PAGE_SCOPE;
+            case "request": return PageContext.REQUEST_SCOPE;
+            case "session": return PageContext.SESSION_SCOPE;
+            case "application": return PageContext.APPLICATION_SCOPE;
+        }
+        
+        throw new JspException("Invalid scope: " + scope);
+    }
+    
+    public String getVar()
+    {
+        return variable;
+    }
+
+    public void setVar(String variable)
+    {
+        this.variable = variable;
+    }
+
+    public String getScope()
+    {
+        return scope;
+    }
+
+    public void setScope(String scope)
+    {
+        this.scope = scope;
+    }
+
+    @Override
+    public Tag getParent()
+    {
+        return this.parent;
+    }
+
+    @Override
+    public void setParent(Tag parent)
+    {
+        this.parent = parent;
+    }
+    
+    public void set(String key, Object value)
+    {
+        this.object.put(key, value);
+    }
+
+    @Override
+    public int doStartTag() throws JspException
+    {
+        this.object = new JSONObject();
+        
+        return EVAL_BODY_INCLUDE;
+    }
+
+    @Override
+    public int doEndTag() throws JspException
+    {
+        if(getParent() instanceof KeyTag)
+        {
+            ((KeyTag)getParent()).setValue(object);
+        }
+        
+        if(variable != null) 
+        {
+            pageContext.setAttribute(this.variable, object, getScopeInt());
+        }
+        
+        return EVAL_PAGE;
+    }
+
+    @Override
+    public void release()
+    {
+        this.object = null;
+        
+        super.release();
+    }
+}
